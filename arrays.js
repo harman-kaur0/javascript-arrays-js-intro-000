49fabb22427e31e5c3c2d67012323cac02bc3a23
balanced-checkpoint-e039 <harmandeepk628@gmail.com>
2021-03-03 04:03:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,10 @@ var tomatoSauceIngredients = [
 ]
 
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(){
+  
+}
+function destructivelyAddElementToBeginningOfArray(){
+  
+}

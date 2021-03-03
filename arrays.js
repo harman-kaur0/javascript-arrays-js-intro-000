a50f6af3e807299c383ca0b6c8123a4c60504f3c
balanced-checkpoint-e039 <harmandeepk628@gmail.com>
2021-03-03 05:41:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -57,7 +57,7 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
 
-function destructivelyRemoveElementFromEndOfArray(){
+function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
   return array
 

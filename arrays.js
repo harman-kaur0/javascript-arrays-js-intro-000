743449b80d6361382a2f1eba1547e21efe838dd9
balanced-checkpoint-e039 <harmandeepk628@gmail.com>
2021-03-03 05:10:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -39,7 +39,7 @@ function addElementToEndOfArray(array, element){
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  return [...array, element]
-  return array.push(element)
+
+  return [...array.push(element)]
 
 }
